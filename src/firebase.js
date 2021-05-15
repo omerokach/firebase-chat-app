ab5dd9538f2eb4e899254de1d13c3303bf5df1a3
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-05-15 13:53:26 +0300

    building the signup page
    based on useContext for the auth of the app,

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+require('dotenv').config();
+
+const app = firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASURMENT_ID
+})
+
+export const auth = app.auth();
+export default app
